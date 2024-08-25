f792ce7ee5ed876216f025b21143ef95398a85f1
Griever255 <JGargiulo95@gmail.com>
2024-08-25 18:44:42 -0500

    more destructuring

---
+++ 8Flow_of_Control/_match/destructuring/enums/src/main.rs
@@ -0,0 +1,39 @@
+// `allow` required to silence warnings because only
+// one variant is used.
+#[allow(dead_code)]
+enum Color {
+    // These 3 are specified solely by their name.
+    Red,
+    Blue,
+    Green,
+    // These likewise tie `u32` tuples to different names: color models.
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+    CMYK(u32, u32, u32, u32),
+}
+
+fn main() {
+    let color = Color::CMYK(5, 10, 50, 85);
+    // TODO ^ Try different variants for `color`
+
+    println!("What color is it?");
+    // An `enum` can be destructured using a `match`.
+    match color {
+        Color::Red => println!("The color is red!"),
+        Color::Blue => println!("The color is blue!"),
+        Color::Green => println!("The color is green!"),
+        Color::RGB(r, g, b) => 
+            println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) =>
+            println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) =>
+            println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) =>
+            println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) =>
+        println!("Cyan: {}, magenta: {}, yellow: {}, key (black): {}!", c, m, y, k),
+        // Don't need another arm because all variants have been examined
+    }
+}
